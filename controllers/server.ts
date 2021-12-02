f5828ec77e4215ba89f91682776bf97d4253e9a0
GloAy <ggloria.ay@gmail.com>
2021-12-02 15:55:28 -0500

    install dependecies, setup first model

---
+++ controllers/server.ts
@@ -0,0 +1,27 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const PORT = process.env.PORT || 3000;
+//const morgan = require("morgan");
+const { sequelize } = require("../models/index");
+// const cors = require("cors");
+// app.use(
+//   cors({
+//     origin: "http://localhost:8080",
+//   })
+// );
+
+//middleware =>
+// app.use(morgan("dev"));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+
+// app.use(express.static(path.join(__dirname, "..", "views")));
+// app.use("/api", require("./api"));
+
+app.listen(PORT, async () => {
+  console.log(`Server running on port ${PORT}`);
+  await sequelize.authenticate();
+  console.log("Database Connected!");
+}),
+  (module.exports = app);
---
+++ models/user.ts
@@ -0,0 +1,50 @@
+"use strict";
+const { Model } = require("sequelize");
+
+
+// These are all the attributes in the User model
+interface UserAttributes {
+  id:string;
+  name:string;
+  email:string;
+  password:string;
+  address: string;
+}
+
+
+module.exports = (sequelize: any, DataTypes: any) => {
+  class User extends Model<UserAttributes> 
+  implements UserAttributes{
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    id!: string;
+    firstName!: string;
+    lastName!: string;
+    email!:string;
+    password!:string;
+    address!:string
+
+    static associate(models: any) {
+      // define association here
+    
+    }
+  }
+  User.init(
+    {
+      firstName: DataTypes.STRING,
+      lastName: DataTypes.STRING,
+      email: DataTypes.STRING,
+      password: DataTypes.STRING,
+      address: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      tableName: "users",
+      modelName: "User",
+    }
+  );
+  return User;
+};
