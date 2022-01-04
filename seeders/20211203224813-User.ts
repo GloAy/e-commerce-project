2929a5dcf5450c43290b519dae7a72b2e9c42905
GloAy <ggloria.ay@gmail.com>
2022-01-04 18:33:45 -0500

    add datatypes to seeders

--- seeders/20211203224813-User.ts
+++ seeders/20211203224813-User.ts
@@ -1,7 +1,7 @@
 "use strict";
 
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
+  up: async (queryInterface: any, Sequelize: any) => {
     await queryInterface.bulkInsert("users", [
       {
         id: 1,
@@ -26,7 +26,7 @@ module.exports = {
     ]);
   },
 
-  down: async (queryInterface, Sequelize) => {
+  down: async (queryInterface: any, Sequelize: any) => {
     /**
      * Add commands to revert seed here.
      *
