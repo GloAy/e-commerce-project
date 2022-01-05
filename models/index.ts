9fc8df46c72aaf722ad643b23340d8c0b8b22e61
GloAy <ggloria.ay@gmail.com>
2022-01-04 19:58:20 -0500

    change start script

--- models/index.ts
+++ models/index.ts
@@ -9,43 +9,18 @@ const config = require(__dirname + "/../config/config.js")[env];
 const db: any = {};
 
 
-// let sequelize: any;
-// if (config.use_env_variable) {
-//   sequelize = new Sequelize(process.env[config.use_env_variable], config);
-// } else {
-//   sequelize = new Sequelize(
-//     config.database,
-//     config.username,
-//     config.password,
-//     config
-//   );
-// }
-
 let sequelize: any;
-if (process.env.NODE_ENV === "production") {
-  sequelize = new Sequelize(process.env.DATABASE_URL, {
-    dialect: "postgres",
-    protocol: "postgres",
-    dialectOptions: {
-      ssl: {
-        sslmode: "require",
-        require: true,
-        rejectUnauthorized: false,
-      },
-    },
-  });
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
-  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, {
-    host: process.env.DB_HOST,
-    pool: {
-      max: 100,
-      min: 0,
-      idle: 200000,
-      // @note https://github.com/sequelize/sequelize/issues/8133#issuecomment-359993057
-      acquire: 1000000,
-    },
-  });
+  sequelize = new Sequelize(
+    config.database,
+    config.username,
+    config.password,
+    config
+  );
 }
+
 
 fs.readdirSync(__dirname)
   .filter((file: string) => {
@@ -54,7 +29,6 @@ fs.readdirSync(__dirname)
     );
   })
   .forEach((file: any) => {
-    //inject sequelize and datatypes
     const model = require(path.join(__dirname, file))(
       sequelize,
       Sequelize.DataTypes
@@ -73,5 +47,7 @@ db.sequelize = sequelize;
 
 
 module.exports = db;
+
+
 
 
